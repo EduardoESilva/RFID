32a00d26a30d6819ce2a56c5d604059bc8f7eeb0
EduardoEliasCMAK <eduardo.silva@cmak.ind.br>
2023-12-07 09:15:03 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace RFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.COM = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbLeituraContinua = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox2);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Location = new System.Drawing.Point(263, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(244, 79);
+            this.groupBox4.TabIndex = 31;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Potência";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17"});
+            this.comboBox2.Location = new System.Drawing.Point(50, 17);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(155, 21);
+            this.comboBox2.TabIndex = 28;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(129, 45);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 25);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "Definir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(50, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 25);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Ler";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.COM);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(244, 79);
+            this.groupBox3.TabIndex = 30;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Conexão";
+            // 
+            // COM
+            // 
+            this.COM.AutoSize = true;
+            this.COM.Location = new System.Drawing.Point(15, 21);
+            this.COM.Name = "COM";
+            this.COM.Size = new System.Drawing.Size(62, 13);
+            this.COM.TabIndex = 3;
+            this.COM.Text = "Porta COM:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "COM1",
+            "COM2"});
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10"});
+            this.comboBox1.Location = new System.Drawing.Point(83, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Tag = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbLeituraContinua);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Location = new System.Drawing.Point(263, 97);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 72);
+            this.groupBox2.TabIndex = 33;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Quando em modo Comandos";
+            // 
+            // cbLeituraContinua
+            // 
+            this.cbLeituraContinua.AutoSize = true;
+            this.cbLeituraContinua.Location = new System.Drawing.Point(7, 30);
+            this.cbLeituraContinua.Name = "cbLeituraContinua";
+            this.cbLeituraContinua.Size = new System.Drawing.Size(105, 17);
+            this.cbLeituraContinua.TabIndex = 1;
+            this.cbLeituraContinua.Text = "Leitura Contínua";
+            this.cbLeituraContinua.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(129, 26);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(76, 25);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Ler TAGs";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 72);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Quando em modo Ativo";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(160, 26);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(76, 25);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Parar";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(81, 26);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 25);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Ler TAGs";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 175);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(495, 141);
+            this.textBox1.TabIndex = 34;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 328);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label COM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbLeituraContinua;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RFID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button11.Enabled = false;
+
+            byte[] arrBuffer = new byte[256];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] arrBuffer = new byte[64];
+
+            String strPort = comboBox1.Text;
+            if (RFID.CFComApi.CFCom_OpenDevice(strPort, 115200))
+            {
+                this.SetText("Conectado\r\n");
+                if (RFID.CFComApi.CFCom_GetDeviceSystemInfo(0xFF, arrBuffer) == false)
+                {
+                    this.SetText("Erro de Conexão\r\n");
+                    //RFID.CFComApi.CFCom_CloseDevice();
+                    //return;
+                }
+            }
+            else
+            {
+                this.SetText("Falha\r\n");
+                return;
+            }
+
+            string str = "", str1 = "";
+            str = String.Format("SoftVer:{0:D}.{0:D}\r\n", arrBuffer[0] >> 4, arrBuffer[0] & 0x0F);
+            this.SetText(str);
+            str = String.Format("HardVer:{0:D}.{0:D}\r\n", arrBuffer[1] >> 4, arrBuffer[1] & 0x0F);
+            this.SetText(str);
+            str = "SN:";
+            for (int i = 0; i < 7; i++)
+            {
+                str1 = String.Format("{0:X2}", arrBuffer[2 + i]);
+                str = str + str1;
+            }
+            str = str + "\r\n";
+            this.SetText(str);
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            RFID.CFComApi.CFCom_CloseDevice();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button6.Enabled = true;
+            button11.Enabled = false;
+            this.SetText("Fechar\r\n");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte bParamAddr = 0;
+            byte[] bValue = new byte[2];
+
+            /*  01: Transport
+                02: WorkMode
+                03: DeviceAddr
+                04: FilterTime
+                05: RFPower
+                06: BeepEnable
+                07: UartBaudRate*/
+            bParamAddr = 0x05;
+
+            if (RFID.CFComApi.CFCom_ReadDeviceOneParam(0xFF, bParamAddr, bValue) == false)
+            {
+                this.SetText("Falha");
+                return;
+            }
+            string str1 = "";
+            str1 = bValue[0].ToString("d2");
+            str1 = "RF:" + str1 + "\r\n";
+
+            comboBox2.SelectedIndex = bValue[0];
+            this.SetText(str1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            byte bParamAddr = 0;
+            byte bValue = 0;
+
+            /*  01: Transport
+                02: WorkMode
+                03: DeviceAddr
+                04: FilterTime
+                05: RFPower
+                06: BeepEnable
+                07: UartBaudRate*/
+            bParamAddr = 0x05;
+            //bValue = 26;   //RF = 26
+
+            bValue = (byte)Convert.ToInt16(comboBox2.SelectedItem);
+
+            if (RFID.CFComApi.CFCom_SetDeviceOneParam(0xFF, bParamAddr, bValue) == false)
+            {
+                this.SetText("Falha");
+                return;
+            }
+            this.SetText("Sucesso");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.SetText("Modo Ativo\r\n");
+            RFID.CFComApi.CFCom_ClearTagBuf();
+            timer1.Interval = 100;
+            timer1.Enabled = true;
+            button6.Enabled = false;
+            button11.Enabled = true;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            button6.Enabled = true;
+            button11.Enabled = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!cbLeituraContinua.Checked)
+            {
+                byte[] arrBuffer = new byte[4096];
+                ushort iNum = 0;
+                ushort iTotalLen = 0;
+                this.SetText("Modo Comandos\r\n");
+                if (RFID.CFComApi.CFCom_InventoryG2(0xFF, arrBuffer, out iTotalLen, out iNum) == false)
+                {
+                    this.SetText("Falha\r\n");
+                    return;
+                }
+                int iTagLength = 0;
+                int iTagNumber = 0;
+                iTagLength = iTotalLen;
+                iTagNumber = iNum;
+                if (iTagNumber == 0) return;
+                int iIndex = 0;
+                int iLength = 0;
+                byte bPackLength = 0;
+                int i = 0;
+
+                for (iIndex = 0; iIndex < iTagNumber; iIndex++)
+                {
+                    bPackLength = arrBuffer[iLength];
+                    string str2 = "";
+                    string str1 = "";
+                    str1 = arrBuffer[1 + iLength + 0].ToString("X2");
+                    str2 = str2 + "Tipo:" + str1 + " ";  //Tag Type
+
+                    str1 = arrBuffer[1 + iLength + 1].ToString("X2");
+                    str2 = str2 + "Ant:" + str1 + " Tag:";  //Ant
+
+                    string str3 = "";
+                    for (i = 2; i < bPackLength - 1; i++)
+                    {
+                        str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                        str3 = str3 + str1 + "";
+                    }
+                    str2 = str2 + str3;
+                    str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                    str2 = str2 + " RSSI:" + str1 + "\r\n";  //RSSI
+                    iLength = iLength + bPackLength + 1;
+                    this.SetText(str2);
+                }
+            }
+            else
+            {
+                timer2.Interval = 300;
+                timer2.Enabled = !timer2.Enabled;
+                cbLeituraContinua.Enabled = !cbLeituraContinua.Enabled;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            byte[] arrBuffer = new byte[4096];
+            int iNum = 0;
+            int iTotalLen = 0;
+            byte bRet = 0;
+
+            bRet = RFID.CFComApi.CFCom_GetTagBuf(arrBuffer, out iTotalLen, out iNum);
+            if (bRet == 1)
+            {
+                this.SetText("Erro de Conexão");
+                return; //DevOut
+            }
+            else if (bRet == 0) return; //No Connect
+            int iTagLength = 0;
+            int iTagNumber = 0;
+            iTagLength = iTotalLen;
+            iTagNumber = iNum;
+            if (iTagNumber == 0) return;
+            int iIndex = 0;
+            int iLength = 0;
+            byte bPackLength = 0;
+            int i = 0;
+
+            for (iIndex = 0; iIndex < iTagNumber; iIndex++)
+            {
+                bPackLength = arrBuffer[iLength];
+                string str2 = "";
+                string str1 = "";
+                str1 = arrBuffer[1 + iLength + 0].ToString("X2");
+                str2 = str2 + "Tipo:" + str1 + " ";  //Tag Type
+
+                str1 = arrBuffer[1 + iLength + 1].ToString("X2");
+                str2 = str2 + "Ant:" + str1 + " Tag:";  //Ant
+
+                string str3 = "";
+                for (i = 2; i < bPackLength - 1; i++)
+                {
+                    str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                    str3 = str3 + str1 + "";
+                }
+                str2 = str2 + str3;
+                str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                str2 = str2 + " RSSI:" + str1 + "\r\n";  //RSSI
+                iLength = iLength + bPackLength + 1;
+                this.SetText(str2);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            byte[] arrBuffer = new byte[4096];
+            ushort iNum = 0;
+            ushort iTotalLen = 0;
+            this.SetText("Modo Comandos\r\n");
+            if (RFID.CFComApi.CFCom_InventoryG2(0xFF, arrBuffer, out iTotalLen, out iNum) == false)
+            {
+                this.SetText("Falha\r\n");
+                return;
+            }
+            int iTagLength = 0;
+            int iTagNumber = 0;
+            iTagLength = iTotalLen;
+            iTagNumber = iNum;
+            if (iTagNumber == 0) return;
+            int iIndex = 0;
+            int iLength = 0;
+            byte bPackLength = 0;
+            int i = 0;
+
+            for (iIndex = 0; iIndex < iTagNumber; iIndex++)
+            {
+                bPackLength = arrBuffer[iLength];
+                string str2 = "";
+                string str1 = "";
+                str1 = arrBuffer[1 + iLength + 0].ToString("X2");
+                str2 = str2 + "Tipo:" + str1 + " ";  //Tag Type
+
+                str1 = arrBuffer[1 + iLength + 1].ToString("X2");
+                str2 = str2 + "Ant:" + str1 + " Tag:";  //Ant
+
+                string str3 = "";
+                for (i = 2; i < bPackLength - 1; i++)
+                {
+                    str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                    str3 = str3 + str1 + "";
+                }
+                str2 = str2 + str3;
+                str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                str2 = str2 + " RSSI:" + str1 + "\r\n";  //RSSI
+                iLength = iLength + bPackLength + 1;
+                this.SetText(str2);
+            }
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox1.Text = this.textBox1.Text + text;
+                this.textBox1.SelectionStart = this.textBox1.Text.Length;
+                this.textBox1.ScrollToCaret();
+            }
+        }
+    }
+}
